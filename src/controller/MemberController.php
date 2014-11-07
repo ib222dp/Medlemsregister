8ea24fe636de98ffe8cc18e2fb970cceb9d3b22a
ib222dp <ib222dp@users.noreply.github.com>
2014-11-07 01:02:14 +0100

    Update MemberController.php

--- src/controller/MemberController.php
+++ src/controller/MemberController.php
@@ -14,7 +14,7 @@ class MemberController {
 	}
 	
 	public function userClickedBoat(){
-		if(array_key_exists("boat", $_GET)){
+		if($this->view->userClickedBoat()){
 			return true;
 		}else{
 			return false;
@@ -25,10 +25,13 @@ class MemberController {
 
 		$memberNo = $this->view->getMemberNo();
 		
+		//Visar fullständig medlemslista
 		if($this->view->userPressedShowFullList()){
 			$ret = $this->view->showFullList($this->memberRepository->getMembers());
+		//Visar kompakt medlemslista	
 		}elseif(empty($memberNo) && !$this->view->userPressedCreateMember() && !$this->view->userPressedCreateMSubmit()) {
 			$ret = $this->view->showCompactList($this->memberRepository->getMembers(), "");
+		//Om användaren vill registrera en ny medlem	
 		}elseif($this->view->userPressedCreateMember()){
 			if($this->view->userPressedCreateMSubmit()){
 				$string =	$this->memberRepository->createMember($this->view->getLastName(), 
@@ -37,6 +40,7 @@ class MemberController {
 			}else{
 				$ret = $this->view->showCreateMemberForm("");
 			}
+		//Om användaren vill redigera en medlems uppgifter	
 		}elseif($this->view->userPressedEditMember()){
 			if($this->view->userPressedEditMSubmit()){
 				$string =	$this->memberRepository->editMember($memberNo, $this->view->getLastName(), 
@@ -45,9 +49,11 @@ class MemberController {
 			}else {
 				$ret = $this->view->showEditMemberForm($this->memberRepository->getMember($memberNo), "");
 			}
+		//Om användaren vill radera en medlem	
 		}elseif($this->view->userPressedDeleteMember()){
 			$string = $this->memberRepository->deleteMember($memberNo);
-			$ret = $this->view->showCompactList($this->memberRepository->getMembers(), $string);
+			$ret = $this->view->showMemberDeletedPage($string);
+		//Visar den medlem som användaren valt	
 		}else{
 			$ret = $this->view->showMember($this->memberRepository->getMember($memberNo), "");
 		}
